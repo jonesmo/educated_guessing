1cc5b22ad707d47ce6b83a5b08400a1ebb157fd3
Molly Jones <mojones.e@gmail.com>
2023-02-26 12:42:46 -0600

    resolve numba version issues

--- src/grains.py
+++ src/grains.py
@@ -9,7 +9,7 @@ def make_one_grain(grain_length, output_file_name, audio_file, filelength, sampl
     ending_sample = starting_sample + grain_length
     grain = audio_file[starting_sample:ending_sample]
 
-    audio_output_path = os.path.join("..", "generated", "grains", output_file_name)
+    audio_output_path = os.path.join("generated", "grains", output_file_name)
 
     if write_out:
         sf.write(audio_output_path, grain, sample_rate, subtype='PCM_24')
--- src/load_audio.py
+++ src/load_audio.py
@@ -1,10 +1,9 @@
 import os
-import numpy as np
 import librosa
 
 # load single audio file
 def load_audio_file(texture_folder, file_name):
-    path_to_audio = os.path.join("../..", texture_folder, file_name)
+    path_to_audio = os.path.join("..", texture_folder, file_name)
 
     audio_file, sr = librosa.load(path_to_audio)
     filelength = len(audio_file)
