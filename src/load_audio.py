acf5503cf98e5eb5aadfd86f1e605da31e638295
Molly Jones <mojones.e@gmail.com>
2023-02-26 13:11:19 -0600

    create load_audio_dir

--- src/load_audio.py
+++ src/load_audio.py
@@ -10,3 +10,22 @@ def load_audio_file(texture_folder, file_name):
     return audio_file, sr, filelength
 
 # load all audio files in directory
+def load_audio_dir(texture_folder):
+    path_to_audio = os.path.join("..", texture_folder)
+
+    files_in_folder = [file for file in os.listdir(path_to_audio) if (os.path.isfile(os.path.join(path_to_audio, file)) and file.endswith(".wav"))]
+    num_audio_files = len(files_in_folder)
+    
+    i = 0
+    audio_buffers = {}
+    for file_name in files_in_folder:
+        audio_file, sr, filelength = load_audio_file(texture_folder, file_name)
+
+        key = i
+        value = { "sample_rate": sr, "file_length": filelength, "data": audio_file }
+        audio_buffers[key] = value
+        i += 1
+    
+    return num_audio_files, audio_buffers
+
+load_audio_dir("texture_2")
